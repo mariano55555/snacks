5c6f338b7aa52193c7199de68e84a498d1769f36
Mariano Paz <jorge.sv@squeaky.com>
2017-11-21 14:54:28 -0600

    pre

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,58 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+/**
+ * PRODUCTS END-POINTS
+ * Adding products
+ * Removing products 
+ * and set their stock quantity.
+ * Obtain a list of all the available products. (INDEX)
+    * * The list should have pagination functionality
+    * * Search through the products by name.
+ * Modify the price of the products (UPDATE)
+ * Save a log of the price updates for a product. (UPDATE)
+ * Liking a product (LIKE)
+*/
+
+
+// USERS ENDPOINTS
+Route::resource('users', 'UsersController', ['except' => ['create', 'edit']]);
+Route::name('verify')->get('users/verify/{token}', 'UsersController@verify');
+Route::name('resend')->get('users/{user}/resend', 'UsersController@resend');
+
+
+//PRODUCTS ENDPOINTS
+Route::resource('products', 'ProductsController',['except' => ['edit', 'create']]);
+Route::post('products/{product}/like', 'ProductsController@like');
+Route::post('products/{product}/stock', 'ProductsController@setStock');
+Route::post('products/{product}/price', 'ProductsController@setPrice');
+
+
+//TRANSACTIONS ENDPOINTS
+Route::resource('transactions', 'InvoicesController',['except' => ['edit', 'create', 'destroy', 'update']]);
+
+
+// TOKEN GENERATOR
+Route::post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
+
+
+/**
+ * Buy a product (INVOICE CREATE)
+ * 
+ *  ---Buying a product should reduce its stock. (INVENTORY-INVOICE)
+ * 
+ * Keep a log of all the purchases (who bought it, how many, when).
+ * 
+ * * The list should be sortable by name (default), and by popularity (likes) 
+
+ 
+
+ 
+ * 
+ */
+
